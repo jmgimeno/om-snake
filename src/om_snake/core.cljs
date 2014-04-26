fae13c291688696922ea708a5657ec49ffa82e4d
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2014-04-26 16:07:20 +0200

    First working game. Lots of things to improve.

--- src/om_snake/core.cljs
+++ src/om_snake/core.cljs
@@ -8,31 +8,27 @@
 
 (enable-console-print!)
 
-(def height 40)
-(def width 40)
-(def size 10)
+(def height 20)
+(def width  20)
+(def size   15)
 (def plank 100)
 
 (def keycodes {37 :left 38 :up 39 :right 40 :down})
-
-(def app-state (atom {:food nil
-                      :snake []
-                      :direction :up}))
 
 (defn random-place-not-in [forbidden]
   (first (drop-while (fn [place] (some #{place} forbidden))
                      (repeatedly (fn [] [(rand-int width)
                                          (rand-int height)])))))
 
-(defn do-place-food [{:keys [snake] :as state}]
-  (om/update! state [:food] (random-place-not-in snake)))
-
-(defn do-init-game [state]
+(defn init-game []
   (let [snake (random-place-not-in [])
         food  (random-place-not-in [snake])]
-    (om/update! state [:food] food)
-    (om/update! state [:snake] [snake])))
+    {:food      food
+     :snake     [snake]
+     :dead      false
+     :direction (rand-nth (vals keycodes))}))
 
+(def app-state (atom (init-game)))
 
 (defn listen [el type]
   (let [out (chan)]
@@ -43,55 +39,67 @@
   (for [[x y] cells]
     [:rect {:x (* x size) :y (* y size) :height size :width size :class type}]))
 
-(defn world [{:keys [food snake]} _]
+(defn world [{:keys [food snake dead]} _]
   (reify
     om/IRender
-    (render [this]
+    (render [_]
       (html
         [:svg {:width (* width size) :height (* width size)}
          (render-cells [food] "food")
-         (render-cells snake "snake")]))))
+         (render-cells snake (if dead "dead" "alive"))]))))
 
 (defn one-step [[x y] direction]
   (condp = direction
-    :left  [(dec x) y]
-    :up    [x (dec y)]
-    :right [(inc x) y]
-    :down  [x (inc y)]))
+    :left  [(dec x)    y   ]
+    :up    [   x    (dec y)]
+    :right [(inc x)    y   ]
+    :down  [   x    (inc y)]))
 
-(defn next-head [head direction]
+(defn next-head [[head & _] direction]
   (let [[x y] (one-step head direction)]
     [(mod x width) (mod y height)]))
 
-(defn next-snake [[head & tail] direction]
-  (into [(next-head head direction)] (butlast tail)))
+(defn grow-snake [{:keys [snake] :as state} next-head]
+  (let [new-snake (into [next-head] snake)
+        new-food (random-place-not-in new-snake)]
+    (assoc state :snake new-snake :food new-food)))
 
-(defn do-move [{:keys [direction] :as state}]
-  (update-in state [:snake] next-snake direction))
+(defn kill-snake [state]
+  (assoc state :dead true))
+
+(defn move-snake [{:keys [snake] :as state} next-head]
+  (assoc state :snake (into [next-head] (butlast snake))))
+
+(defn game-step [{:keys [snake food direction] :as state}]
+  (let [new-head (next-head snake direction)]
+    (cond
+      (= new-head food)        (grow-snake state new-head)
+      (some #{new-head} snake) (kill-snake state)
+      :else                    (move-snake state new-head))))
 
 (defn root [state _]
   (reify
     om/IWillMount
     (will-mount [_]
       (let [keyboard-channel (listen js/window "keydown")]
-        (do-init-game state)
         (go (loop [step-channel (timeout plank)]
               (alt!
                 keyboard-channel ([e c]
                                     (when-let [direction (keycodes (.-keyCode e))]
                                       (om/update! state [:direction] direction))
-                                    (recur (timeout plank)))
+                                    (recur step-channel))
                 step-channel ([e c]
-                                    (om/transact! state do-move)
-                                    (recur (timeout plank))))))))
+                              (when-not (:dead @state)
+                                (om/transact! state game-step)
+                                (recur (timeout plank)))))))))
 
     om/IRender
     (render [_]
       (html [:div
              [:h1 "Welcome to Snake !!"]
              [:div
-              (str "Snake length: " (count (:snake state))
-                   " Direction: " (:direction state))]
+              (str "Snake length: " (count (:snake state)) " direction: " (:direction state)
+                   (when (:dead state) " ENDGAME !!"))]
              (om/build world state)]))))
 
 (om/root
