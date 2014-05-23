b5a0654d8ca4b3fbe326c2e800686102d7a8e358
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2014-05-23 12:01:44 +0200

    Upsated library versions and created dev and release profiles.

--- project.clj
+++ project.clj
@@ -3,19 +3,27 @@
   :url "http://github.com/jmgimeno/om-snake"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [om "0.6.1"]]
+                 [org.clojure/clojurescript "0.0-2227"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [om "0.6.2"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
   :cljsbuild { 
-    :builds [{:id "om-snake"
+    :builds [{:id "dev"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "om_snake_dev.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}
+             {:id "release"
               :source-paths ["src"]
               :compiler {
                 :output-to "om_snake.js"
-                :output-dir "out"
-                :optimizations :none
-                :source-map true}}]})
+                :optimizations :advanced
+                :pretty-print false
+                :preamble ["react/react.min.js"]
+                :externs ["react/externs/react.js"]}}]})
